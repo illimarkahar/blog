281daf5c824e7458762a0e789c3757b44a8325fe
Illimar Kahar <illimar.kahar@khk.ee>
2014-01-30 10:40:15 +0200

    (Add single post viewing functionality

---
+++ views/posts/posts_view.php
@@ -0,0 +1,9 @@
+
+    <div class="span8">
+        <h1><?=$post['post_subject']?></h1>
+        <p><?=$post['post_text']?></p>
+        <div>
+            <span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label"><?=$post['username']?></span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+        </div>
+        <hr>
+    </div>
